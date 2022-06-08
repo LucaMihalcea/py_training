8ccf57d2c75a17332f25b54fd6bd3635dbb4077c
Ionut Mihalcea <ionut.mihalcea@movenext.fr>
2022-06-08 07:51:13 +0200

    refactor: extract function for median computation

--- remove_extrems/remove_extrems.py
+++ remove_extrems/remove_extrems.py
@@ -2,21 +2,28 @@ from sort_algos import sort
 
 
 def remove_extrems(series, setting):
-    if series is None:                                      #processing of the particular case None
+    if series is None:  # processing of the particular case None
         return []
-    if len(series) == 1: return series                      #processing of the particular case where the series contains a single data
-    sort.naive(series)
-    total = len(series)
-    median_pos = total // 2
+    if len(series) == 1: return series  # processing of the particular case where the series contains a single data
     data_tested = 0
-    for i in range(total):                                  #loop on each of the data
+    median_pos, median_value = median(series)
+    for i, _ in enumerate(series):  # loop on each of the data
         test = series[data_tested] - series[median_pos]
         data_tested += 1
-        if test >= setting:                                 #test if the data is judged as extreme
+        if test >= setting:  # test if the data is judged as extreme
             series.remove(series[data_tested - 1])
-    return series                                           #return the new series without any extreme data
+    return series  # return the new series without any extreme data
 
 
-
+def median(series):
+    """
+    Compute the median
+    :param series: a list of integers
+    :return: the index of the median value, the median value of the series
+    """
+    sort.naive(series)
+    total = len(series)
+    median_index = total // 2
+    return median_index, series[median_index]
 
 
